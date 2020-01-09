665ded4b36a0475f76e3920c13383c064d51e78c
Rachel Saunders <rachelsaundersBA@gmail.com>
2020-01-09 13:08:39 +0000

    Initial Commit
    Hello World is printed in the console

---
+++ Assets/SampleScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleScript : MonoBehaviour
+{
+
+    public int numberToDisplay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SayHelloWorld();
+    }
+
+    private void SayHelloWorld()
+    {
+        for(int i=0; i < numberToDisplay; i++)
+        {
+            Debug.Log("Hello World!");
+        }
+    }
+   
+}
